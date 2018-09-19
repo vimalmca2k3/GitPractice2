cb7f08e22fbfab8f0ec1e98362fbb2181309b984
Vimal Patel <vimal.patel@soti.net>
2018-09-19 14:45:12 -0400

    initializing repo

---
+++ src/FirstClass.java
@@ -0,0 +1,11 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("This is the first class in master branch");
+		System.out.println("This is the first class initilzing repo and creating original repo");
+	}
+
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,10 @@
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is the Second class in master branch");
+		System.out.println("This is the Second class initilzing repo and creating original repo");
+	}
+
+}
